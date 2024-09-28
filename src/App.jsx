98f1d90c8d1a7e29478714376fe384a2dee94618
Levi Francis <levifrancis15@gmail.com>
2024-09-28 18:20:36 +0100

    add connect wallet and display balance

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import './App.css'
+import useWalletConnect from './hooks/useWalletConnect';
+
+function App() {
+  
+  const [address, setAddress] = useState("");
+  const [otherBalance, setOtherBalance] = useState(null);
+
+  const { 
+    account, 
+    connectedBalance, 
+    chainId, 
+    error, 
+    connectWallet, 
+    disconnectWallet, 
+    getBalance,
+    handleBalanceReset } = useWalletConnect();
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (address) {
+      const balance = await getBalance(address);
+      
+      setOtherBalance(balance)
+    }
+  };
+
+  return (
+    <>
+      <div>
+      {!account ? (
+        <button onClick={connectWallet} style={{ 
+          padding: '1rem', 
+          border: '1px solid gray', 
+          borderRadius: '1rem',
+          cursor: 'pointer',
+        }}>Connect Wallet</button>
+      ) : (
+        <div>
+          <p>Connected Account: <span style={{ color: 'greenyellow' }}>{account}</span></p>
+          <p>Chain ID: {chainId}</p>
+          <p>Balance: {connectedBalance}</p>
+          <button
+            onClick={() => {
+              setOtherBalance(null)
+              setAddress('')
+              handleBalanceReset()
+            }}
+            className="px-6 py-2 bg-white text-black font-semibold rounded-lg hover:bg-gray-200"
+          >
+            Reset My Balance
+          </button>
+
+          <div className="py-4">
+            <form onSubmit={handleSubmit} style={{ padding: '1rem'}} >
+              <input
+                type="text"
+                placeholder="Enter Address"
+                style={{ padding: '0.5rem' }}
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="p-2 border border-white bg-black text-white rounded-md mr-4"
+              />
+              {/* <div>{otherBalance}</div> */}
+              <button
+                type="submit"
+                className="px-6 py-2 bg-white text-black font-semibold rounded-lg hover:bg-gray-200"
+              >
+                Get Balance
+              </button>
+            </form>
+          </div>
+
+          <div>
+            <button onClick={disconnectWallet} style={{ 
+              padding: '1rem', 
+              border: '1px solid red', 
+              borderRadius: '1rem',
+              backgroundColor: 'red',
+              cursor: 'pointer',
+            }}>Disconnect Wallet</button>
+          </div>
+        </div>
+      )}
+
+      <div>{error}</div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/hooks/useWalletConnect.js
@@ -0,0 +1,125 @@
+import { ethers } from 'ethers';
+import React, { useEffect, useState } from 'react'
+
+const useWalletConnect = () => {
+
+    const [account, setAccount] = useState(null);
+    const [chainId, setChainId] = useState(null);
+    const [connectedBalance, setConnectedBalance] = useState(null);
+    const [error, setError] = useState(null);
+
+    const ethereum = window.ethereum;
+
+    const connectWallet = () => {
+        if (ethereum) {
+          ethereum
+            .request({ method: 'eth_requestAccounts' })
+            .then(async (accounts) => {
+                // console.log("ACCOUNTS:::", accounts)
+                setAccount(accounts[0])
+                getBalance(accounts[0])
+              console.log(`Account::: ${accounts[0]}`);
+            })
+            .catch((error) => {
+              setError(error.message)
+              console.error(
+                `Error fetching accounts: ${error.message}.
+                Code: ${error.code}. Data: ${error.data}`
+              );
+            });
+    
+            ethereum
+            .request({ method: 'eth_chainId' })
+            .then((chainId) => {
+              setChainId(chainId);
+            //   console.log(`hexadecimal string: ${chainId}`);
+            })
+            .catch((error) => {
+              console.error(`Error fetching chainId: ${error.code}: ${error.message}`);
+            });
+        } else {
+          setError('You have no wallet installed. Please install MetaMask.');
+          return;
+        }
+    }
+
+    const disconnectWallet = () => {
+        setAccount(null);
+        setChainId(null);
+    }
+
+    const getBalance = async (account) => {
+        if (!ethereum) {
+          console.log("No Ethereum provider found");
+          return;
+        }
+    
+        try {
+          const balance = await ethereum.request({
+            method: "eth_getBalance",
+            params: [account, "latest"],
+          });
+
+          console.log("Balance", balance)
+    
+          const balanceInWei = ethers.toBigInt(balance);
+          const balanceInEth = ethers.formatEther(balanceInWei)
+    
+          console.log("Balance in ETH:", balanceInEth);
+        // console.log("Balance in ETH:", connectedBalance);
+    
+            setConnectedBalance(balanceInEth);
+            return balanceInEth;
+        } catch (error) {
+          console.error("Error",error);
+        }
+    };
+
+    const handleBalanceReset = async () => {
+        if(account) {
+          const balance = await getBalance(account);
+    
+          setConnectedBalance(balance);
+        }
+    }
+
+    useEffect(() => {
+        if (ethereum) {
+    
+          const handleAccountsChanged = (accounts) => {
+            if (accounts.length > 0) {
+              setAccount(accounts[0]);
+              getBalance(account[0]);
+            } else {
+              setAccount(null);
+            }
+          };
+    
+          const handleChainChanged = (chainId) => {
+            setChainId(chainId);
+            getBalance(account)
+          };
+    
+          ethereum.on('accountsChanged', handleAccountsChanged);
+          ethereum.on('chainChanged', handleChainChanged);
+    
+          return () => {
+            ethereum.removeListener('accountsChanged', handleAccountsChanged);
+            ethereum.removeListener('chainChanged', handleChainChanged);
+          };
+        }
+      }, [ethereum]);
+
+    return { 
+        account, 
+        connectedBalance,
+        chainId, 
+        error, 
+        connectWallet, 
+        disconnectWallet,
+        getBalance,
+        handleBalanceReset
+    }
+}
+
+export default useWalletConnect
